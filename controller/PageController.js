bef4cf140611293166afa46475e29ac8c5cf4721
TopalBugrahan <76047788+TopalBugrahan@users.noreply.github.com>
2022-05-30 16:38:00 +0300

    commit

--- controller/PageController.js
+++
@@ -1,8 +0,0 @@
-
-exports.getIndex=(req,res)=>{
-    res.render("index")
-}
-
-exports.getDarkWebKiller=(req,res)=>{
-    res.render("darkwebkiller")
-}
--- deneme.js
+++
@@ -1,4 +0,0 @@
-for (let i=0,j=3;i<6,j<10;i++,j++)
-{
-    console.log(i,j)
-}
